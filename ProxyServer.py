f7ed4a85115d9852d7d70e68df212f6b33418439
dreamATD <690293245@qq.com>
2018-10-08 23:30:35 +0800

    Correct it.

--- ProxyServer.py
+++ ProxyServer.py
@@ -1,7 +1,8 @@
 from socket import *
+from urllib.parse import urlsplit
 import sys
 
-
+hostn = ''
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 # Fill in start.
 tcpSerSock.bind(('', 6555))
@@ -15,8 +16,8 @@ while True:
     message = tcpCliSock.recv(4096).decode()
 
     # Extract the filename from the given message
-    print(message.split()[1])
-    filename = message.split()[1].partition("/")[2]
+    filename = message.split()[1].partition('/')[2]
+    print(filename)
     fileExist = 'false'
     filetouse = '/' + filename
     try:
@@ -35,9 +36,14 @@ while True:
         # Error handling for file not found in cache
     except IOError:
         if fileExist == 'false':
+            res_url = urlsplit(filename)
             # Create a socket on the proxyserver
             c = socket(AF_INET, SOCK_STREAM)
-            hostn = filename.replace('www.', '', 1)
+
+
+            if res_url.path.find('.com') != -1 or res_url.path.find('.cn') != -1:
+                hostn = res_url.path
+
             print('Host Name: ', hostn)
             try:
                 # Connect to the socket to port 80
@@ -46,20 +52,36 @@ while True:
                 # Fill in end.
                 # Create a temporary file on this socket and ask port 80 for the file requested by the client
 
-                c.send(("GET " + "http://" + filename + " HTTP/1.0\r\n\r\n").encode())
+                command = message.split('\r\n')
+                if res_url.path == hostn:
+                    p = 'http://' + hostn + '/'
+                else:
+                    p = 'http://' + hostn + '/' + res_url.path
+                local = '127.0.0.1:6555'
+                # command[5] = 'Referer: http:/' + command[5][command[5].find(local) + len(local):]
+                # command = '\r\n'.join(('GET ' + p + ' HTTP/1.1', 'Host: ' + hostn, '\r\n'.join(command[2:])))
+                # command = command.replace('keep-alive', 'close')
+                command = 'GET ' + p + ' HTTP/1.0\r\n\r\n'
+
+                print(command)
+                c.send(command.encode())
 
                 msg = ''
                 while True:
-                    bufs2c = c.recv(1024)
+                    bufs2c = c.recv(65536)
                     if not bufs2c:
                         break
-                    print(bufs2c.decode())
+                    # print(bufs2c.decode())
                     msg += bufs2c.decode()
 
+                print()
                 print(msg)
+                print()
+
                 tcpCliSock.sendall(msg.encode())
             except:
                 print('Illegal request')
+            c.close()
         else:
             # HTTP response message for file not found
             # Fill in start.
